8adc4428665a19311a7d19cc06dbdebc6d3a0cc0
NZSteel <40599959+NZSteel@users.noreply.github.com>
2018-09-04 23:17:28 +0100

    Models and admin area improved

--- investmentrecorder/admin.py
+++ investmentrecorder/admin.py
@@ -2,11 +2,36 @@ from django.contrib import admin
 
 # Register your models here.
 
-from  investmentrecorder.models import Investment, InvestmentCapital, InvestmentExpense, InvestmentRevenue, InvestmentValuation, ExternalUpdater
+from investmentrecorder.models import \
+    Investment, \
+    InvestmentCapital, \
+    InvestmentExpense, \
+    InvestmentRevenue, \
+    InvestmentValuation, \
+    ExternalUpdater,  \
+    Currency
 
-admin.site.register(Investment)
-admin.site.register(InvestmentCapital)
+
+class InvestmentAdmin(admin.ModelAdmin):
+    list_filter = ('short_name', 'investment_type', 'currency', 'description')
+    list_display = ('short_name', 'investment_type', 'currency', 'description')
+
+
+class InvestmentCapitalAdmin(admin.ModelAdmin):
+    list_filter = ('investment', 'purchase_date', 'liquidate_date')
+    list_display = ('investment', 'purchase_date', 'liquidate_date')
+
+
+class ExternalUpdaterAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Investment, InvestmentAdmin)
+admin.site.register(InvestmentCapital, InvestmentCapitalAdmin)
 admin.site.register(InvestmentExpense)
 admin.site.register(InvestmentRevenue)
 admin.site.register(InvestmentValuation)
-admin.site.register(ExternalUpdater)
+admin.site.register(ExternalUpdater, ExternalUpdaterAdmin)
+admin.site.register(Currency)
+
+
--- investmentrecorder/models.py
+++ investmentrecorder/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 # Create your models here.
@@ -29,7 +30,9 @@ class Investment(models.Model):
         choices=INVESTMENT_TYPE,
         help_text='Enter the broad category of investment'
     )
-    currency = models.CharField(max_length=3)
+    currency = models.ForeignKey(
+        'Currency',
+        on_delete=models.PROTECT)
     external_updater = models.ForeignKey(
         'ExternalUpdater',
         on_delete=models.SET_NULL,
@@ -38,40 +41,108 @@ class Investment(models.Model):
         max_length=100,
         null=True,
         blank=True)
+    total_invested = models.DecimalField(
+        max_digits=11,
+        decimal_places=2,
+        null=True,
+        blank=True)
+    total_returned = models.DecimalField(
+        max_digits=11,
+        decimal_places=2,
+        null=True,
+        blank=True)
+    total_number_of_units = models.DecimalField(
+        max_digits=17,
+        decimal_places=8,
+        null=True,
+        blank=True)
 
     class Meta:
         ordering = ['investment_type', 'short_name']
+
+    def get_absolute_url(self):
+        return reverse('investment-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.short_name}'
 
 
 class InvestmentCapital(models.Model):
     # Fields
     investment = models.ForeignKey('Investment', on_delete=models.CASCADE)
-    purchase_date = models.DateField(help_text='Enter the date this tranche of investment was made')
-    liquidate_date = models.DateField(null=True, help_text='Enter the date this tranche of investment was made')
-    purchase_amount = models.DecimalField(max_digits=11, decimal_places=2)
-    liquidate_amount = models.DecimalField(max_digits=11, decimal_places=2, null=True)
-    currency = models.CharField(max_length=3)
+    purchase_date = models.DateField(
+        null=True,
+        blank=True,
+        help_text='Enter the date this tranche of investment was made')
+    liquidate_date = models.DateField(
+        null=True,
+        blank=True,
+        help_text='Enter the date this tranche of investment was liquidated')
+    number_of_units = models.DecimalField(
+        max_digits=17,
+        decimal_places=8,
+        null=True,
+        blank=True)
+    price_per_unit = models.DecimalField(
+        max_digits=19,
+        decimal_places=10,
+        null=True,
+        blank=True)
+    purchase_amount = models.DecimalField(
+        max_digits=11,
+        decimal_places=2,
+        null=True,
+        blank=True)
+    liquidate_amount = models.DecimalField(
+        max_digits=11,
+        decimal_places=2,
+        null=True,
+        blank=True)
+    currency = models.ForeignKey(
+        'Currency',
+        on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.id} ({self.investment.short_name})'
 
 
 class InvestmentRevenue(models.Model):
     investment = models.ForeignKey('Investment', on_delete=models.CASCADE)
     date = models.DateField(help_text='Enter the date the revenue was received')
-    currency = models.CharField(max_length=3)
+    currency = models.ForeignKey(
+        'Currency',
+        on_delete=models.PROTECT)
     amount = models.DecimalField(max_digits=11, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.id} ({self.investment.short_name})'
 
 
 class InvestmentExpense(models.Model):
     investment = models.ForeignKey('Investment', on_delete=models.CASCADE)
     date = models.DateField(help_text='Enter the date the revenue was received')
-    currency = models.CharField(max_length=3)
+    currency = models.ForeignKey(
+        'Currency',
+        on_delete=models.PROTECT)
     amount = models.DecimalField(max_digits=11, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.id} ({self.investment.short_name})'
 
 
 class InvestmentValuation(models.Model):
     investment = models.ForeignKey('Investment', on_delete=models.CASCADE)
     date = models.DateField(help_text='Enter the date the revenue was received')
     amount = models.DecimalField(max_digits=11, decimal_places=2)
+    price_per_unit = models.DecimalField(
+        max_digits=19,
+        decimal_places=10,
+        null=True,
+        blank=True)
     status = models.CharField(max_length=10, help_text='Actual or derived interim value')
+
+    def __str__(self):
+        return f'{self.id} ({self.investment.short_name})'
 
 
 class ExternalUpdater(models.Model):
@@ -80,3 +151,15 @@ class ExternalUpdater(models.Model):
     webservice_login = models.CharField(max_length=100, null=True, blank=True)
     webservice_password_plaintext = models.CharField(max_length=100, null=True, blank=True)
     webservice_token = models.CharField(max_length=500, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Currency(models.Model):
+    short_code = models.CharField(max_length=3)
+    symbol = models.CharField(max_length=1)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.short_code} {self.name}'
